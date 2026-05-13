ceacc573f8e96aab03b85ec2d27bfeb0d34e7458
mdsrhsn <mdsr.h01@gmail.com>
2026-05-13 15:34:35 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2958,8 +2958,7 @@ def purchases_summary():
     </div>
     """
 
-    return layout("Purchases Summary", "purchases", body)
-
+return layout("Purchases Summary", "pur", body)
 
 # ═══════════════════════════════════════════════════════════════════
 # ROUTE 2: Vendor Detail Page
